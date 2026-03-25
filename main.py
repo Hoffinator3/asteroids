caab0088f4e9eb684abd8e6619a92d89717c77f6
Hoffinator3 <123334411+Hoffinator3@users.noreply.github.com>
2026-03-25 16:46:37 -0400

    forgot to save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+
 from logger import log_state
 def main():
     pygame.init()
@@ -7,10 +8,12 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    
     while True:
         log_state()
         for event in pygame.event.get():
-            pass
+            if event.type == pygame.QUIT:
+                return
 
         screen.fill("black")
         pygame.display.flip()
