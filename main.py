dff569f7dd5faca938d5fcb06583cb5983b2f318
Hoffinator3 <123334411+Hoffinator3@users.noreply.github.com>
2026-03-25 19:43:14 -0400

    added player

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import pygame
-import constants
-
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+import circleshape
+from player import Player
 from logger import log_state
+
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
     clock = pygame.time.Clock()
     dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    
 
     while True:
         log_state()
@@ -18,10 +24,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        player = Player(x,y)
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+
         milliseconds = clock.tick(60)
         dt = milliseconds / 1000
+
+
 
 
 if __name__ == "__main__":
