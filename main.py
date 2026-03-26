ac1fd9772515aaa6d13d9a9a1cd5b7eb004d8742
Hoffinator3 <123334411+Hoffinator3@users.noreply.github.com>
2026-03-25 21:53:40 -0400

    changed loop to use groups

--- main.py
+++ main.py
@@ -14,9 +14,14 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
-    player = Player(x,y)
+    Player(x,y)
+
 
     while True:
         log_state()
@@ -24,13 +29,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        
-
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
-        pygame.display.flip()
 
+        updatable.update(dt)
+        for drawn in drawable:
+            drawn.draw(screen)
+
+        pygame.display.flip()
 
         milliseconds = clock.tick(60)
         dt = milliseconds / 1000
