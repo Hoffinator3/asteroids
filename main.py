6e789e811ee8dbe8ccc278cf15cbf244b63821fa
Hoffinator3 <123334411+Hoffinator3@users.noreply.github.com>
2026-03-26 18:34:59 -0400

    added asteroids and asteroidfields

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_TURN_SPEED
 import circleshape
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from logger import log_state
 
 def main():
@@ -16,12 +18,16 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     Player(x,y)
 
+    AsteroidField()
 
     while True:
         log_state()
